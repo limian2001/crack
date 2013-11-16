c7b868ff8910f33f55d1141bd23d5e0f93f0f642
limian2001 <ki87ujml@gmail.com>
2013-11-16 15:19:45 -0600

    updated compressStr and isRotation

--- compress1_5Str.java
+++ compress1_5Str.java
@@ -1,16 +1,25 @@
-
+/* Cracking 1.5 
+ * Implement a method to perform basic string compression using the counts
+ * of repeated characters. For example, the string aabcccccaaa would become
+ * a2b1c5a3. If the "compressed" string would not become smaller than the 
+ * original string, your method should return the original string. */
 public class compress1_5Str {
 
 	public static void main(String[] args)
 	{
-		String a="sssssjjjja";
+		String a="aaabc";
 		System.out.println(compress(a));
 	}
+/* write a method to produce compressed string */
 	public static String compress(String s)
 	{
+		/* if the input is null, return -1*/
+		if(s.length()==0)
+			return "-1";
 		String mystr="";
 		char last=s.charAt(0);
 		int count=1;
+		/*    */
 		for(int i=1;i<s.length();i++){
 			if(s.charAt(i)==last)
 			{	count++;  }
@@ -20,7 +29,11 @@ public class compress1_5Str {
 				count=1;
 			}
 		}
-			return mystr+last+count;
+			String compString=mystr+last+count;
+			if(compString.length()>s.length())
+				return s;
+			else
+				return compString;
 
 }
 }
--- isRotation.java
+++ isRotation.java
@@ -1,4 +1,4 @@
-
+/* Cracking 1.8  */
 public class isRotation {
 	/*contat S1 and S1, judge S2 is subString of S1+S1. String.Contains is also ok */
 	public static boolean isRotation(String s1,String s2){
